3ac239de1ea5fff9a15b4ce5a7c4bc576ee77af1
Ivanushka13 <davydoff@MacBook-Air-Ivan.local>
2022-03-11 12:07:08 +0300

    Hometask

---
+++ 03module/ДЗ16/API/API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("/api/[controller]")]
+    public class ProductsController
+    {
+        public ProductsController()
+        {
+        }
+
+        private static List<Product> products = new List<Product>(new[] {
+        new Product() { Id = 1, Name = "Notebook", Price = 100000 },
+        new Product() { Id = 2, Name = "Car", Price = 2000000 },
+        new Product() { Id = 3, Name = "Apple", Price = 30 },
+        });
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => products;
+
+        [HttpGet("{id}")]           // параметр для маршрутизации
+        public IActionResult Get(int id)
+        {
+            var product = products.SingleOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(product);
+        }
+
+
+    }
+}
---
+++ 03module/ДЗ16/API/API/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+    }
+}
