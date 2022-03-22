599ce777f82cc57098a7e3aab7462174e3ffcda9
Ivanushka13 <davydoff@MacBook-Air-Ivan.local>
2022-03-22 21:40:43 +0300

    Hometask

---
+++ 03module/05seminar/Seminar5/Seminar5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Seminar5
+{
+    interface IFunction
+    {
+        public double Function(double x);
+    }
+
+    public delegate double Calculate(double input);
+
+    class F : IFunction
+    {
+        Calculate calculate;
+
+        public F(Calculate calculate)
+        {
+            this.calculate = calculate;
+        }
+
+        public double Function(double x) 
+        {
+            return calculate(x);
+        }
+
+    }
+
+    class G
+    {
+        F first;
+        F second;
+
+        public G(F f1, F f2)
+        {
+            first = f1;
+            second = f2;
+        }
+
+        public double GF(double x0)
+        {
+            return first.Function(second.Function(x0));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            G g = new G(new F(x => x * x - 4), new F(x => Math.Sin(x)));
+            for (double i = 0; i < Math.PI; i += Math.PI / 16)
+                Console.WriteLine(g.GF(i).ToString("F3"));
+        }
+    }
+}
